09267399ba266a2ed76ebace7c6ca426a25c4929
jayashree26umale <58214576+jayashree26umale@users.noreply.github.com>
2020-11-11 18:04:17 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -14,7 +14,8 @@ const app = require('../index.js')
 describe('GET /' ,function(){
     it('respond with hello world',function(done){
         request(app).get('/').expect('Hello World!',done);
+        process.exit()
     })
 })
 
-process.exit()
+
