42a5f33822fb582f0eaaa5b3cf2c64f1bc9c5a0d
jayashree26umale <58214576+jayashree26umale@users.noreply.github.com>
2020-11-11 18:13:48 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -12,10 +12,12 @@ const request=require('supertest')
 const app = require('../index.js')
 
 describe('GET /' ,function(){
-    it('respond with hello world',function(done){
+    before('respond with hello world',function(done){
         request(app).get('/').expect('Hello World!',done);
        
     })
+    it(function(){
+        process.exit()})
 })
 
 
